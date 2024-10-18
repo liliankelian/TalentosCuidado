71f6bb62513b3f01f5b5ede9afab43ba9599daaa
lilikelian <lilielili@gmail.com>
2024-10-18 17:52:51 -0300

    correçoes para o rodar no AWS

--- src/cuidador/infraestructure/in-database/typeOrm.config.ts
+++ src/cuidador/infraestructure/in-database/typeOrm.config.ts
@@ -4,11 +4,11 @@ import { CuidadorEntity } from "./entities/cuidador.entity";
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: +process.env.DB_PORT,
-  username: process.env.DB_USERNAME,
-  password: String(process.env.DB_PASSWORD),
-  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST || 'localhost',
+  port: +process.env.DB_PORT || 5432,
+  username: process.env.DB_USERNAME || 'postgres',
+  password: String(process.env.DB_PASSWORD) || 'pg12345678',
+  database: process.env.DB_DATABASE || 'talentosos',
   logging: true,
   migrations: [__dirname + '/migrations/*.{ts,js}'],
   entities: [CuidadorEntity],
